a7bcf981756611ca7e385f34513899dab367c6fe
Bilge <134232792+goksubilge@users.noreply.github.com>
2024-04-04 15:34:43 +0300

    init

--- src/test/java/com/example/fruit/FruitApplicationTests.java
+++ src/test/java/com/example/fruit/FruitApplicationTests.java
@@ -1,10 +1,10 @@
-package com.wit.Spring.generall;
+package com.example.fruit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class FruitApplicationTests {
 
 	@Test
 	void contextLoads() {
