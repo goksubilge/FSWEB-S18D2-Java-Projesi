7c48db3b15bd2706f71baac4e85460d083467525
Bilge <134232792+goksubilge@users.noreply.github.com>
2024-04-06 18:24:54 +0300

    add Slf4j to GlobalExceptionHandler

--- src/main/java/com/example/fruit/exceptions/GlobalExceptionHandle.java
+++ src/main/java/com/example/fruit/exceptions/GlobalExceptionHandle.java
@@ -1,5 +1,6 @@
 package com.example.fruit.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,7 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandle {
 
@@ -19,6 +20,7 @@ public class GlobalExceptionHandle {
     @ExceptionHandler
     public ResponseEntity<PlantErrorResponse> handleException(Exception exception){
         PlantErrorResponse plantErrorResponse = new PlantErrorResponse(HttpStatus.BAD_REQUEST.value(), exception.getMessage(), LocalDateTime.now());
+        log.error("EXCEPTION OCCURED: " + exception.getMessage());  // önemli log, iyi bir alışkanlık olur hiç handle etmediğimiz şeyleri.
         return new ResponseEntity<>(plantErrorResponse, HttpStatus.BAD_REQUEST);
     }
 
@@ -27,8 +29,9 @@ public class GlobalExceptionHandle {
     public ResponseEntity handleBindException(MethodArgumentNotValidException exception){
         List errorList = exception.getBindingResult().getFieldErrors()
                 .stream().map(fieldError -> {
+                    log.error(fieldError.getField(), fieldError.getDefaultMessage());
                     Map<String, String> errorMap = new HashMap<>();
-                    errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+                    errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());  // bu log olmasa da olur.
                     return errorMap;
                 }).collect(Collectors.toList());
 
